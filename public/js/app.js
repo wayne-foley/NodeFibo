d7557fa8e4ef27ebf663a7dba87beb3b05ed1bab
Phani Raj <phanirajuyn@gmail.com>
2014-11-11 15:48:04 -0800

    added funnel highcharts, removed old tables

--- public/js/app.js
+++ public/js/app.js
@@ -28,31 +28,36 @@ function getFunnelStats(rec, own){
     Owner_PersonId : own,
    })
     .done(function(results) {
-      var chart_html = "";
+      var seriesdata = [];
+      var sevendayseries = [];
       var stages = ["Lead", "Phone Screen", "Interview", "Offer", "Accepted"];
       var stage_icons = ["lead", "phone", "interview", "offer", "accepted"];
       //TODO: Dynamically pull stage names from backend.  Hardcoding for now
       for(i=0; i<stages.length; i++){
-        chart_html += "<td style='width:20%;' data-toggle='tooltip' data-placement='auto' title='"+stages[i]+"'><i class='fa tp-"+stage_icons[i]+"' style='font-size:20px;'></i><div style='font-size:12px;'>"+results.overallFunnelStats.funnel[i];
-        if(i != stages.length-1) {
-          chart_html += "<td style='font-size:20px;'><div class='arrow_box' style='width:28px;height:14px;position:relative;top:-2px' data-toggle='tooltip' data-placement='auto' title='Percent that move on'><span style='font-size:11px;position:absolute;left:3px;top:0px;'>" + Math.round(results.overallFunnelStats.aggragate[i+1]) + "%";
-          chart_html +="</span></div></td>";
+        var datum = [];
+        var stageName = stages[i];
+        if( i !=stages.length-1) {
+          stageName += "("+Math.round(results.overallFunnelStats.aggragate[i+1])+"%)";
         }
+        datum.push(stageName);
+        datum.push(results.overallFunnelStats.funnel[i]);
+        seriesdata.push (datum);
       }
-      $("#funnel_target_all").removeClass("loading");
-      $("#funnel_row_all").html(chart_html);
 
       //Now for 7 day
-      chart_html = "";
       for(i=0; i<stages.length; i++){
-        chart_html += "<td style='width:20%;' data-toggle='tooltip' data-placement='auto' title='"+stages[i]+"'><i class='fa tp-"+stage_icons[i]+"' style='font-size:20px;'></i><div style='font-size:12px;'>"+results.weeklyFunnelStats.funnel[i];
-        if(i != stages.length-1) {
-          chart_html += "<td style='font-size:20px;'><div class='arrow_box' style='width:28px;height:14px;position:relative;top:-2px' data-toggle='tooltip' data-placement='auto' title='Percent that move on'><span style='font-size:11px;position:absolute;left:3px;top:0px;'>" + Math.round(results.weeklyFunnelStats.aggragate[i+1]) + "%";
-          chart_html +="</span></div></td>";
+        var datum = [];
+        var stageName = stages[i];
+        if( i !=stages.length-1) {
+          stageName += "("+Math.round(results.weeklyFunnelStats.aggragate[i+1])+"%)";
         }
+        datum.push(stageName);
+        datum.push(results.overallFunnelStats.funnel[i]);
+        sevendayseries.push(datum);
       }
-      $("#funnel_target_seven").removeClass("loading");
-      $("#funnel_row_seven").html(chart_html);
+
+      renderHighChartsFunnel('funnelChart', 'Historical (all)', seriesdata);
+      renderHighChartsFunnel('funnelChart7Days', 'Historical (7-Day)', sevendayseries);
     });
 }
 
@@ -62,23 +67,63 @@ function saveNote(candidateId, note) {
 }
 
 
+function renderHighChartsFunnel(chartid, title, seriesdata) {
+
+  $(function () {
+
+      $('#'+chartid).highcharts({
+          chart: {
+              type: 'funnel',
+              marginRight: 100
+          },
+          title: {
+              text: title,
+              x: -50
+          },
+          plotOptions: {
+              series: {
+                  dataLabels: {
+                      enabled: true,
+                      format: '<b>{point.name}</b> ({point.y:,.0f})',
+                      color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                      softConnector: true
+                  },
+                  neckWidth: '30%',
+                  neckHeight: '25%'
+
+                  //-- Other available options
+                  // height: pixels or percent
+                  // width: pixels or percent
+              }
+          },
+          legend: {
+              enabled: false
+          },
+          series: [{
+              name: 'Candidate funnel',
+              data: seriesdata
+          }]
+      });
+  });
+}
+
 function saveState() {
   var $btn = $("#btnSaveStage").button('saving');
   //Sam is lazy and just making three Ajax calls vs. refractoring handlers.  Sorry guys.
   $.post("/candidate/changeowner", {
       id : $("#candidate_value").val(),
       personId : $("#owner_change_value").val(),
-      
+
     })
     .done(function(build) {
     });
-  
+
   var date = moment($(".datetimepicker").data('date'));
 
   $.post("/candidate/changeDueDate", {
       id : $("#candidate_value").val(),
       dueDate : date.format('YYYY-MM-DD')
-      
+
     })
     .done(function(build) {
     });
@@ -86,7 +131,7 @@ function saveState() {
   $.post("/candidate/changestate", {
       id : $("#candidate_value").val(),
       stateId : $("#stage_value").val(),
-      
+
     })
     .done(function(build) {
       $('#changeStateModel').modal('hide');
