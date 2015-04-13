0b1882d507b5d9aeea4cf862e97d2c1a30bfbc0b
Sam Gazitt <gazitt@gmail.com>
2015-04-13 15:28:25 -0700

    trying to update unit testing framework

---
+++ tests/test.js
@@ -0,0 +1,24 @@
+var http = require('http'),
+    should = require('should'),
+    sysInfo = require('../lib/sysinfo.js'),
+    keyProcessor = require('../lib/keyprocessor.js'),
+    hock = require('hock');
+
+describe('node-env/sdk/environment', function (done) {
+  var context = {};
+
+  before(function (done) {
+    done();
+  }),
+
+  it("Can list system environment variables", function () {
+    var env = sysInfo.sysInfo();
+    should.exist(env);
+    env.length.should.be.equal(5);
+  });
+
+  it("Key processor leaves values unchanged", function () {
+    var result = keyProcessor.procKey('Name', 'VALUE');
+    result.should.be.equal('VALUE');
+  });
+});
